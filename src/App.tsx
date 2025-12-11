7cbeecc597c008acf99e9e233ea76be3812354eb
jiwonnayoung <lny1203@naver.com>
2025-12-11 17:41:34 +0900

    chore: router 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,28 @@
-import "./App.css";
+import "@/App.css";
+import { Outlet, Route, Routes } from "react-router";
+import IndexPage from "@/pages/index-page";
+import SignInPage from "@/pages/sign-in-page";
+import SignUpPage from "@/pages/sign-up-page";
+
+function AuthLayout() {
+  return (
+    <div>
+      <header>Auth!</header>
+      <Outlet />
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div>
-      {/** 1. 타이포그래피 */}
-      <div className="text-xs text-red-500">text-xs</div>
-      <div className="text-sm text-[rgb(100,30,200)]">text-sm</div>
-      <div className="text-lg">text-lg</div>
-      <div className="text-xl font-bold">text-xl</div>
-      <div className="text-2xl font-extrabold">text-2xl</div>
-      <div className="text-[13px] font-black">text-13px</div>
+    <Routes>
+      <Route path="/" element={<IndexPage />} />
 
-      {/** 2. 백그라운드 컬러 */}
-      <div className="bg-amber-500">amber-500</div>
-
-      {/** 3. 사이즈 */}
-      <div className="h-20 w-full bg-blue-500">box</div>
-
-      {/** 4. 여백 */}
-      <div className="m-5 h-50 w-50 bg-red-400 p-5">
-        <div className="h-full w-full bg-blue-400"></div>
-      </div>
-
-      {/** 5. 보더 */}
-      <div className="m-5 rounded-md border-2 border-red-500">border</div>
-
-      {/** 6. 플렉스 컨테이너 */}
-      <div className="flex flex-row items-center justify-evenly">
-        <div className="h-10 w-10 border">1</div>
-        <div className="h-20 w-10 flex-1 border">2</div>
-        <div className="h-30 w-10 border">3</div>
-        <div className="h-40 w-10 border">4</div>
-      </div>
-    </div>
+      <Route element={<AuthLayout />}>
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+      </Route>
+    </Routes>
   );
 }
 
