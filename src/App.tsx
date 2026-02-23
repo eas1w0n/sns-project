f855afccf5a1c7db3ec686bc44ed7603225fd3e9
jiwonnayoung <lny1203@naver.com>
2026-02-23 15:53:05 +0900

    feat: 세션 데이터 로딩  상태 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { useEffect } from "react";
 import RootRoute from "./root-route";
 import supabase from "./lib/supabase";
-import { useSetSession } from "./store/session";
+import { useIsSessioinLoaded, useSetSession } from "./store/session";
 
 export default function App() {
   const setSession = useSetSession();
+  const isSessionLoaded = useIsSessioinLoaded();
 
   useEffect(() => {
     supabase.auth.onAuthStateChange((event, session) => {
       setSession(session);
     });
   }, []);
+
+  if (!isSessionLoaded) return <div>로딩 중...</div>;
 
   return <RootRoute />;
 }
