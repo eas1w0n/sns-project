08433e4fdb8ccd2ceb42cb89b5fb2080d5c9d07b
jiwonnayoung <lny1203@naver.com>
2026-03-11 16:44:35 +0900

    feat: 포스트 수정 기능 구현

--- src/components/post/create-post-button.tsx
+++ src/components/post/create-post-button.tsx
@@ -1,12 +1,12 @@
-import { useOpenPostEditorModal } from "@/store/post-editor-modal";
+import { useOpenCreatePostModal } from "@/store/post-editor-modal";
 import { PlusCircleIcon } from "lucide-react";
 
 export default function CreatePostButton() {
-  const openPostEditorMdoal = useOpenPostEditorModal();
+  const openCreatePostModal = useOpenCreatePostModal();
 
   return (
     <div
-      onClick={openPostEditorMdoal}
+      onClick={openCreatePostModal}
       className="bg-muted text-muted-foreground cursor-pointer rounded-xl px-6 py-4"
     >
       <div className="flex items-center justify-between">
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "sonner";
 
 const queryClient = new QueryClient({
@@ -17,6 +18,7 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
       <Toaster />
       <App />
     </QueryClientProvider>
